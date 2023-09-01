dec7067753feb8f46eba6b73dfafff1ebe005974
KimDev88 <kys2407@gmail.com>
2023-09-01 23:26:07 +0900

    * properties 외부주입 방식으로 변경  * persistence.xml datasource 외부주입 방식으로 변경

--- src/main/java/com/bdpick/service/ShopService.java
+++ src/main/java/com/bdpick/service/ShopService.java
@@ -81,4 +81,65 @@ public class ShopService {
                 .exceptionally(Mono::error)
                 .toCompletableFuture().join();
     }
+
+//    public Mono<Boolean> checkRegisterNumber(@NonNull Shop shop) {
+//
+//        String registerNumber = shop.getRegisterNumber();
+//
+//        // request parameter 설정
+//        Map<String, Object> bodyMap = new HashMap<>();
+//        List<String> bnoList = new ArrayList<>();
+//
+//        bnoList.add(registerNumber);
+//        bodyMap.put("b_no", bnoList);
+//
+//        // 공곰 API 호출 설정
+//        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory("https://api.odcloud.kr/api/nts-businessman/v1/");
+//        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+//        WebClient client = WebClient
+//                .builder()
+//                .uriBuilderFactory(factory)
+//                .build();
+//
+//        return shopRepository.findShopByRegistNumber(registerNumber)
+//                .hasElement()
+//                .flatMap(aBoolean -> {
+//                    // 해당 사업자번호가 존재할 경우
+//                    if (aBoolean) {
+//                        response.setError("이미 존재하는 사업자번호 입니다.", false);
+//                    } else {
+//                        return client
+//                                .post()
+//                                .uri("status?serviceKey=" + openApiToken)
+//                                .body(Mono.just(bodyMap), Map.class)
+//                                .exchangeToMono(clientResponse -> clientResponse.bodyToMono(Map.class))
+//                                .map(o -> {
+//                                    Optional<Map<String, Object>> result = Optional.ofNullable(o);
+//                                    Optional<List<Map<String, Object>>> dataList = result.map(stringObjectMap -> (List<Map<String, Object>>) stringObjectMap.get("data"));
+//                                    Optional<Map<String, Object>> resultMap = dataList.stream().findAny().orElseGet(ArrayList::new).stream().findFirst();
+//                                    Optional<String> bSttCd = resultMap.map(stringObjectMap -> (String) stringObjectMap.get("b_stt_cd"));
+//                                    if (bSttCd.isEmpty() || bSttCd.get().equals("")) {
+//                                        response.setError("사업자 번호가 유효하지 않습니다.", false);
+//                                    } else {
+//                                        switch (bSttCd.get()) {
+//                                            // 계속사업자
+//                                            case "01":
+//                                                response.setData(true);
+//                                                break;
+//                                            // 휴업사업자
+//                                            case "02":
+//                                                response.setError("휴업 사업자 입니다.", false);
+//                                                break;
+//                                            // 폐업자
+//                                            case "03":
+//                                                response.setError("폐업 사업자 입니다.", false);
+//                                                break;
+//                                        }
+//                                    }
+//                                    return response;
+//                                });
+//                    }
+//                    return Mono.just(response);
+//    }
+
 }
