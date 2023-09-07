276930f7748916392b30cd1f3fe7435dbbe3e16c
KimDev88 <kys2407@gmail.com>
2023-09-08 00:12:42 +0900

    * 미사용 파일 정리

--- src/main/java/com/bdpick/repository/UserRepository.java
+++ src/main/java/com/bdpick/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.bdpick.repository;
 
 import com.bdpick.domain.entity.User;
-import io.smallrye.mutiny.Uni;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.reactive.mutiny.Mutiny;
 import org.hibernate.reactive.stage.Stage;
 import org.springframework.stereotype.Repository;
 
@@ -39,22 +37,5 @@ public class UserRepository {
                     System.out.println("user = " + user);
                     return user;
                 });
-//        return factory.openSession(session -> {
-//            return session.createQuery("select u from User u where email = :email", User.class)
-//                    .setParameter("email", email)
-//                    .getSingleResultOrNull()
-//                    .thenApply(user -> {
-//                        System.out.println("user = " + user);
-//                        return user;
-//                    });
-//        }).toCompletableFuture().join();
-
-//        return session.createQuery("select u from User u where email = :email", User.class)
-//                .setParameter("email", email)
-//                .getSingleResultOrNull()
-//                .thenApply(user -> {
-//                    System.out.println("user = " + user);
-//                    return user;
-//                }).toCompletableFuture().join();
     }
 }
